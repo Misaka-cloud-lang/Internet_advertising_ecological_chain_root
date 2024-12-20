bedb67f62b36fbd7cf6fd60090f79023f797df6c
goldfish9901 <745727212@qq.com>
2024-12-20 13:49:42 +0800

    jyl test: localize configuration to fit server context

--- advertisement_system/src/main/java/org/example/advertisement_system/AdvertisementSystemApplication.java
+++ advertisement_system/src/main/java/org/example/advertisement_system/AdvertisementSystemApplication.java
@@ -1,9 +1,12 @@
 package org.example.advertisement_system;
 
+import org.example.advertisement_system.controller.AdvertisementAdminController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"org.example.advertisement_system"}, basePackageClasses = {AdvertisementAdminController.class})
 public class AdvertisementSystemApplication {
 
     public static void main(String[] args) {
